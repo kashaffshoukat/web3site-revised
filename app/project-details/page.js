fe6cf7e946fcbfbd51eb19c6d459a05ca3be2b4b
kashaf shoukat <139368562+kashaffshoukat@users.noreply.github.com>
2025-08-19 12:20:54 +0500

    Update page.js

--- app/project-details/page.js
+++ app/project-details/page.js
@@ -16,7 +16,7 @@ export default function ProjectDetails() {
                                         <span className="boder" />
                                         <div className="img-box">
                                             <div className="image">
-                                                <img className="mask" src="/assets/common/project_1.png" alt="" />
+                                                <img className="mask" src="/assets/project_1.png" alt="" />
                                                 <div className="shape">
                                                     <img src="/assets/common/shape2.png" alt="" />
                                                 </div>
@@ -199,7 +199,7 @@ export default function ProjectDetails() {
                                             </li>
                                         </ul>
                                         <div className="image mb30">
-                                            <img className="boder-20" src="/assets/details_1.jpg" alt="" />
+                                            <img className="boder-20" src="/assets/common/details_1.jpg" alt="" />
                                         </div>
                                         <div className="box">
                                             <h4 className="heading mb10">1. Project overview</h4>
