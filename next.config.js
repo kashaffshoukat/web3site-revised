bb20f96fd1531ac05702c13a7f4a17115404e5c5
kashaf shoukat <139368562+kashaffshoukat@users.noreply.github.com>
2025-08-19 12:25:01 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    // 👇 add all external domains you load images from
+    domains: ["res.cloudinary.com", "ipfs.io", "gateway.pinata.cloud"],
+    // If your images are only in /public, you don’t need this
+  },
+}
 
 module.exports = nextConfig
